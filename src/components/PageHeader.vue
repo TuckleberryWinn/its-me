d9e178fbf8bb3fc568ab10255e7dba39a63b9d24
TuckleberryWinn <63176475+TuckleberryWinn@users.noreply.github.com>
2025-03-13 20:57:05 -0600

    Clean up Vue template stuff #Back2Basics

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Some Header Nonsense</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: rgb(75, 147, 210);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main">
+    <h2>Home</h2>
   </main>
 </template>
+
+<style scoped>
+.main {
+  background-color: gray;
+}
+</style>
