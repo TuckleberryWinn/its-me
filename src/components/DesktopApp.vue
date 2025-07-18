1651d80547b025fee699f35b326514b5decbf9da
TuckleberryWinn <63176475+TuckleberryWinn@users.noreply.github.com>
2025-07-17 21:32:56 -0600

    Window styling

--- src/components/DesktopApp.vue
+++ src/components/DesktopApp.vue
@@ -15,10 +15,10 @@ const props = defineProps({
 			</div>
 			<div class="control-buttons">
 				<div class="control-minimize">-</div>
-				<div class="control-maximize">+</div>
 				<div class="control-close">X</div>
 			</div>
 		</div>
+		<div class="app-body"></div>
 	</div>
 </template>
 
@@ -28,7 +28,6 @@ const props = defineProps({
 	min-width: 16rem;
 	min-height: 9rem;
 	height: auto;
-	aspect-ratio: 16/9;
 	background-color: grey;
 	border: 0.155rem solid #00ffd4;
 	border-radius: 0.5rem 0.5rem 0 0;
@@ -36,7 +35,13 @@ const props = defineProps({
 }
 .app-header {
 	height: 2rem;
-	background: linear-gradient(0deg,rgba(90, 0, 138, 1) 8%, rgba(66, 145, 214, 1) 35%, rgba(0, 255, 212, 1) 65%, #00ffd4 100%);;
+	background: linear-gradient(
+		0deg,
+		rgba(90, 0, 138, 1) 8%,
+		rgba(66, 145, 214, 1) 35%,
+		rgba(0, 255, 212, 1) 65%,
+		#00ffd4 100%
+	);
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
@@ -51,9 +56,6 @@ const props = defineProps({
 	font-size: 1.6rem;
 }
 
-.app-title {
-}
-
 .control-buttons {
 }
 
@@ -64,15 +66,34 @@ const props = defineProps({
 	border: 2px solid rgb(79, 7, 142);
 	border-radius: 0.4rem;
 	margin-left: 0.4rem;
-  background-color: rgba(1, 110, 187, 0.7);
+	background-color: rgba(1, 110, 187, 0.7);
 }
 
 .control-buttons > .control-close {
-  background-color: rgba(255, 7, 7, 0.7);
+	background-color: #f826fc;
 }
 .control-buttons > div > span {
 	height: 1rem;
 	width: 1rem;
 	background-color: black;
+}
+
+.app-title h1 {
+	font-weight: 600;
+	text-shadow:
+		-1px 1px 1px #9de0f9,
+		-2px 2px 1px #9de0f9,
+		1px -1px 1px #9de0f9,
+		2px -2px 1px #9de0f9,
+		1px 1px 1px #6bd7ff,
+		2px 2px 1px #6bd7ff,
+		-1px -1px 1px #6bd7ff,
+		-2px -2px 1px #6bd7ff;
+	overflow: visible;
+	color: #f826fc;
+}
+
+.app-body {
+	aspect-ratio: 16/9;
 }
 </style>
