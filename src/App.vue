683c642548f7baa06ce1d5a0aa723e86d2facd57
Tuckleberry Winn <63176475+TuckleberryWinn@users.noreply.github.com>
2025-08-29 18:04:27 -0600

    Remove all bits of failed cursor attempt

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { startScene, endScene } from './managers/threeSceneManager';
-import { onUnmounted, ref } from 'vue';
-
-import CursorSource from '@/assets/ui/32x_cursorSheet.png';
+import { onUnmounted } from 'vue';
 
 startScene();
 
 onUnmounted(() => {
 	endScene();
 });
-
-const cursorSize = 32;
-
-const cursorSpriteSheet = new Image();
-cursorSpriteSheet.src = CursorSource;
-
-const cursorBuffer = ref('');
-cursorBuffer.value =
-	'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAIAAAC0Ujn1AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAEDSURBVEhLtZJBEoMwDAP7lr6nn+0LqUGChsVOwoGdvTSSNRz6Wh7jxvT7+wn9Y4LZae0e+rXLeBqjh45rBtOYgy4V9KYxlOpqRjmNiY4+uJBP41gOI5BM40w620AknTVwGgfSWQMK0tnOaRpV6ewCatLZxn8aJemsAGXp7JhGLBX1wYlUtE4jkIpnwKGM9xeepG7mwblMpl2/CUbCJ7+6CnQzAw5lvD/8DxGIpbMClKWzdjpASTq7gJp0tnGaDlCVzhpQkM52OB3gQDrbQCSdNSTTAc7kMAL5dIDjjj64UE4HmEh1NaM3HWAIulQwmA4wd+i4ZjwdYDR00GqWsyPrizLD76QCPOHqP2cAAAAAElFTkSuQmCC';
-
-const cursorTracker = (event) => {
-	const computed = Object.fromEntries([...event.target.computedStyleMap().entries()]);
-	console.log(computed.cursor);
-};
-
-document.body.addEventListener('mousemove', cursorTracker);
 </script>
 
 <template>
@@ -44,11 +26,5 @@ document.body.addEventListener('mousemove', cursorTracker);
 	min-height: 100%;
 	max-width: 100%;
 	max-height: 100%;
-}
-
-.page {
-	height: 100vh;
-	width: 100vw;
-	cursor: v-bind(`url('${CursorSource}') `), auto;
 }
 </style>
