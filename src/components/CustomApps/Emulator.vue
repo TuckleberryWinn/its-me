98bcf86261f0679236e196d5eabc65bb939bb09d
Tuckleberry Winn <63176475+TuckleberryWinn@users.noreply.github.com>
2025-12-28 10:55:06 -0700

    Emulator dropdown layout

--- src/components/CustomApps/Emulator.vue
+++ src/components/CustomApps/Emulator.vue
@@ -2,12 +2,38 @@
 import MobileView from '@/components/MobileBio.vue';
 import { ref } from 'vue';
 
-const currentView = ref('mobile');
+const virtualPhones = [
+	{
+		name: 'Spamdung Fallacy',
+		height: 740,
+		width: 360,
+	},
+	{
+		name: 'eyePWN',
+		height: 874,
+		width: 403,
+	},
+];
+
+const currentView = ref(virtualPhones[1]);
 </script>
 
 <template>
 	<div class="emulator">
-		<div class="control-panel"></div>
+		<div class="control-panel">
+			<select v-model="currentView">
+				<optgroup label="Phone">
+					<option
+						v-for="phone in virtualPhones"
+						:key="phone.name"
+						:value="phone"
+					>
+						{{ phone.name }}
+					</option>
+				</optgroup>
+			</select>
+			<pre>currentView: {{ currentView }}</pre>
+		</div>
 		<div class="app-container">
 			<div class="app-display">
 				<MobileView></MobileView>
@@ -18,24 +44,32 @@ const currentView = ref('mobile');
 
 <style scoped>
 .emulator {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
 }
 
 .control-panel {
 	height: 2.5rem;
-  background: linear-gradient(180deg,
-  rgba(40, 27, 67, 1) 0%,
-  rgba(40, 27, 67, 0.4) 14%,
-  rgba(40, 27, 67, 0.5) 84%,
-  rgba(40, 27, 67, 1) 92%,
-  rgba(0, 255, 212, 1) 100%);
+	background: linear-gradient(
+		180deg,
+		rgba(40, 27, 67, 1) 0%,
+		rgba(40, 27, 67, 0.4) 14%,
+		rgba(40, 27, 67, 0.5) 84%,
+		rgba(40, 27, 67, 1) 92%,
+		rgba(0, 255, 212, 1) 100%
+	);
+}
+
+.control-panel > select {
+	background: none;
+	color: aqua;
+	height: 100%;
 }
 
 .app-container {
 	align-items: center;
-  margin: auto
+	margin: auto;
 }
 
 .app-display {
