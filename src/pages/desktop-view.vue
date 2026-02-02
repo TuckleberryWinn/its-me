073ca8d0c9dc98730ef26ee50cf8b54f50dcd8e9
Tuckleberry Winn <63176475+TuckleberryWinn@users.noreply.github.com>
2026-02-02 08:25:07 -0700

    Change page layout to start away from desktop

---
+++ src/pages/desktop-view.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import useWindowManager from '@/composables/useWindowManager';
+const { windows, taskbarTabs, appList } = useWindowManager();
+
+import MobileBio from '@/components/MobileBio.vue';
+import DesktopApp from '@/components/DesktopApp.vue';
+import TaskbarApp from '@/components/TaskbarApp.vue';
+import DesktopIcon from '@/components/DesktopIcon.vue';
+import Taskbar from '@/components/Taskbar.vue';
+import ScanlineShader from '@/components/ScanlineShader.vue';
+
+import BlurryGlass from '@/components/AltShaders/BlurryGlass.vue';
+import BoxStyles from '@/components/AltShaders/BoxStyles.vue';
+</script>
+
+<template>
+	<div id="top-level">
+		<MobileBio class="view-mobile"></MobileBio>
+		<div class="view-full">
+			<main class="main">
+				<BlurryGlass />
+				<BoxStyles />
+				<DesktopIcon
+					v-for="app in appList"
+					:key="app.appID"
+					v-bind="app"
+				/>
+				<div id="desktop-app-container">
+					<h1
+						style="color: white"
+						v-for="window in windows"
+					>
+						{{ window }}
+					</h1>
+					<DesktopApp
+						v-for="window in windows"
+						:key="window.appID"
+						v-bind="window"
+					>
+					</DesktopApp>
+				</div>
+				<Taskbar>
+					<div id="taskbar-app-container">
+						<TaskbarApp
+							v-for="tab in taskbarTabs"
+							:key="tab.appID"
+							v-bind="tab"
+						></TaskbarApp>
+					</div>
+				</Taskbar>
+			</main>
+		</div>
+		<ScanlineShader />
+	</div>
+</template>
+
+<style scoped>
+.main {
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	align-content: flex-start;
+}
+
+#taskbar-app-container {
+	display: flex;
+	flex-wrap: nowrap;
+}
+
+#top-level {
+	height: 100dvh;
+	width: 100dvw;
+}
+
+.view-full {
+	display: none;
+}
+.view-mobile {
+	display: flex;
+}
+
+@media (min-width: 640px) {
+	.view-full {
+		display: block;
+	}
+	.view-mobile {
+		display: none;
+	}
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,92 +1,18 @@
-<script setup lang="ts">
-import useWindowManager from '@/composables/useWindowManager';
-const { windows, taskbarTabs, appList } = useWindowManager();
-
-import MobileBio from '@/components/MobileBio.vue';
-import DesktopApp from '@/components/DesktopApp.vue';
-import TaskbarApp from '@/components/TaskbarApp.vue';
-import DesktopIcon from '@/components/DesktopIcon.vue';
-import Taskbar from '@/components/Taskbar.vue';
-import ScanlineShader from '@/components/ScanlineShader.vue';
-
-import Cursor from '@/components/Cursor.vue';
-import BlurryGlass from '@/components/AltShaders/BlurryGlass.vue';
-import BoxStyles from '@/components/AltShaders/BoxStyles.vue';
+<script lang="ts" setup>
+import { swapScene } from '@/managers/threeSceneManager';
 </script>
 
 <template>
-	<div id="top-level">
-		<MobileBio class="view-mobile"></MobileBio>
-		<div class="view-full">
-			<main class="main">
-				<BlurryGlass />
-				<BoxStyles />
-				<DesktopIcon
-					v-for="app in appList"
-					:key="app.appID"
-					v-bind="app"
-				/>
-				<div id="desktop-app-container">
-					<h1
-						style="color: white"
-						v-for="window in windows"
-					>
-						{{ window }}
-					</h1>
-					<DesktopApp
-						v-for="window in windows"
-						:key="window.appID"
-						v-bind="window"
-					>
-					</DesktopApp>
-				</div>
-				<Taskbar>
-					<div id="taskbar-app-container">
-						<TaskbarApp
-							v-for="tab in taskbarTabs"
-							:key="tab.appID"
-							v-bind="tab"
-						></TaskbarApp>
-					</div>
-				</Taskbar>
-			</main>
-			<Cursor />
-		</div>
-		<ScanlineShader />
+	<div class="about">
+		<h2>About</h2>
+		<RouterLink to="/desktop-view"
+			><button @click="swapScene('ScreenView')">Start Computer</button></RouterLink
+		>
 	</div>
 </template>
 
-<style scoped>
-.main {
-	display: flex;
-	flex-direction: column;
-	flex-wrap: wrap;
-	align-content: flex-start;
-}
-
-#taskbar-app-container {
-	display: flex;
-	flex-wrap: nowrap;
-}
-
-#top-level {
-	height: 100dvh;
-	width: 100dvw;
-}
-
-.view-full {
-	display: none;
-}
-.view-mobile {
-	display: flex;
-}
-
-@media (min-width: 640px) {
-	.view-full {
-		display: block;
-	}
-	.view-mobile {
-		display: none;
-	}
+<style>
+.about {
+	background-color: rgba(92, 83, 83, 0.489);
 }
 </style>
