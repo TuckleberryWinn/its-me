56ce84aa4096c0d7b8f7c662804b56298bfdce12
Tuckleberry Winn <63176475+TuckleberryWinn@users.noreply.github.com>
2026-04-27 01:26:15 -0600

    Global cursor sheet implemented

--- src/components/GlobalCursor.vue
+++ src/components/GlobalCursor.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { xCursorOffset, yCursorOffset } from '@/managers/sceneGlobals';
-import cursorSheet from '@/assets/ui/48xCursorSet-Sheet.png';
+import cursorSheet from '@/assets/ui/64x_global_cursors-Sheet.png';
 
 const frameInterval = ref(0);
 
-const currentCursor = ref(0);
+enum Cursor {
+	debug = 0,
+	default = 1,
+	inspect = 2,
+	arrow_right = 3,
+	arrow_left = 4,
+	arrow_up = 5,
+	arrow_down = 6,
+}
+const currentCursor = ref<Cursor>(Cursor.default);
 
 setInterval(() => {
 	frameInterval.value += 1;
@@ -27,6 +36,5 @@ setInterval(() => {
 .cursor {
 	width: 64px;
 	aspect-ratio: 1;
-	background-color: gray;
 }
 </style>
