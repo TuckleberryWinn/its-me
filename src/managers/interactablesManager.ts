93ac86638d490274c5c9b54805c78af1af555da1
Tuckleberry Winn <63176475+TuckleberryWinn@users.noreply.github.com>
2026-05-04 21:01:39 -0600

    Update interactablesManager.ts
    Start type docs.  Moving to finish on another machine

--- src/managers/interactablesManager.ts
+++ src/managers/interactablesManager.ts
@@ -1,5 +1,21 @@
 import * as useTooltip from '@/composables/useTooltip';
 
+/**
+ * @property {string}  [0] - Outline shader to trigger.
+ * @property {string}  [1] - Cursor to show.
+ * @property {string}  [2] - Tooltip to display.
+ */
+// [0] == {'sceneNavigation' | 'objectInspect'}
+// [1] == {	debug | default | inspect | arrow_right | arrow_left | arrow_up | arrow_down }
+
+const interactables: Record<string, string[]> = {
+	Computer_CRT: ['sceneNavigation', 'arrow_up'],
+};
+
+const focusInteractable = () => {};
+
+const unfocusInteractable = () => {};
+
 export const hoverInteractableUI = (prompt: string, direction: string) => {
 	useTooltip.addUIPrompt(prompt, direction);
 };
